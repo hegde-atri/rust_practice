3c0ee94d5eee58f2add29709c29d35daf7eff40b
hegde-atri <iamatrihegde@outlook.com>
2023-01-10 17:36:27 +0000

    Reverse integer

---
+++ leetcode/reverse_integer/src/main.rs
@@ -0,0 +1,39 @@
+// Given a signed 32-bit integer x, return x with its digits reversed.
+// If reversing x causes the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1],
+// then return 0. Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+use std::ops::Neg;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn reverse(x: i32) -> i32 {
+        if x.is_negative() {
+            (x * -1)
+                .to_string()
+                .chars()
+                .rev()
+                .collect::<String>()
+                .parse()
+                .unwrap_or(0)
+                .neg()
+        } else {
+            x.to_string()
+                .chars()
+                .rev()
+                .collect::<String>()
+                .parse()
+                .unwrap_or(0)
+        }
+    }
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {}
+}
