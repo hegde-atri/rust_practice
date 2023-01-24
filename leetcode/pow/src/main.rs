e12ab8b103f3aa616f91cfb76791d7506bac9ca8
hegde-atri <iamatrihegde@outlook.com>
2023-01-24 14:06:58 +0000

    pow(x, y) finished

---
+++ leetcode/pow/src/main.rs
@@ -0,0 +1,30 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn my_pow(x: f64, n: i32) -> f64 {
+        if n == 0 {
+            1.0
+        } else if n == 1 {
+            x
+        } else {
+            x.powf(n.into())
+        }
+    }
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(Solution::my_pow(2.000000, 10), 1024.0000);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(Solution::my_pow(2.000000, -2), 0.2500);
+    }
+}
