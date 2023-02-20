086633b203b9e72816c378154cd19aee256cf511
Atri Hegde <iamatrihegde@outlook.com>
2023-02-20 18:21:41 +0000

    (chore) init new problem

--- general/calculate_pi/src/main.rs
+++ general/calculate_pi/src/main.rs
@@ -1,12 +1,17 @@
+use num_format::{Locale, ToFormattedString};
 use std::time::Instant;
 
 use rand::Rng;
 
-fn main(){
-    run(1_000_000);
+fn main() {
+    let mut iterations: i64 = 10_000_000;
+    loop {
+        run(iterations);
+        iterations *= 10;
+    }
 }
 
-fn run(iterations: i32) {
+fn run(iterations: i64) {
     let mut num_circle = 0.0;
     let mut num_total = 0.0;
     let mut rng = rand::thread_rng();
@@ -23,5 +28,9 @@ fn run(iterations: i32) {
     let pi = 4.0 * (num_circle / num_total);
     let elapsed_time = now.elapsed();
     println!("Calculated value of pi is: {}", pi);
-    println!("Time taken: {} seconds.", elapsed_time.as_secs_f32());
+    println!(
+        "Time taken: {} seconds for {} iterations.",
+        elapsed_time.as_secs_f32(),
+        iterations.to_formatted_string(&Locale::en)
+    );
 }
